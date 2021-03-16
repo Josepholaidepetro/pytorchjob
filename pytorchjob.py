4e76bac81e1fcc8fb8bfad63b7b6f7234f61fc4a
Josepholaidepetro <73533435+Josepholaidepetro@users.noreply.github.com>
2021-03-16 03:51:09 +0100

    Add files via upload

---
+++ pytorchjob.py
@@ -0,0 +1,199 @@
+import argparse
+import logging
+import os
+
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+from torch.optim.lr_scheduler import StepLR
+from torchvision import datasets, transforms
+
+logging.getLogger().setLevel(logging.INFO)
+WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 1))
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 28x28x1 image 
+        self.conv1 = nn.Conv2d(1, 64, 3, 1) 
+        # 26x26x64 ----> pooling -----> 13x13x64
+        self.conv2 = nn.Conv2d(64, 128, 3, 1)
+        # 11x11x128 -----> pooling -----> 5x5x128 
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(3200, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def should_distribute():
+    return dist.is_available() and WORLD_SIZE > 1
+
+
+def is_distributed():
+    return dist.is_available() and dist.is_initialized()
+
+
+def percentage(value):
+    return "{: 5.1f}%".format(100.0 * value)
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            logging.info(
+                f"Epoch: {epoch} ({percentage(batch_idx / len(train_loader))}) - Loss: {loss.item()}"
+            )
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum batch losses
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    logging.info(
+        f"Test accuracy: {correct}/{len(test_loader.dataset)} ({percentage(correct / len(test_loader.dataset))})"
+    )
+
+    # Log metrics for Katib
+    logging.info("loss={:.4f}".format(test_loss))
+    logging.info("accuracy={:.4f}".format(float(correct) / len(test_loader.dataset)))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="PyTorch F_MNIST Training Job")
+    
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        metavar="N",
+        help="Number of epochs to train",
+    )
+
+    parser.add_argument(
+        "--no-cuda",
+        action="store_true",
+        default=False,
+        help="Disables CUDA (GPU) training",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=1, metavar="S", help="Random seed (default: 1)"
+    )
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="Number of training batches between status log entries",
+    )
+    parser.add_argument(
+        "--save-model",
+        action="store_true",
+        default=False,
+        help="Whether to save the trained model",
+    )
+
+    if dist.is_available():
+        parser.add_argument(
+            "--backend",
+            type=str,
+            help="Distributed backend",
+            choices=[dist.Backend.GLOO, dist.Backend.NCCL, dist.Backend.MPI],
+            default=dist.Backend.GLOO,
+        )
+
+    args, _ = parser.parse_known_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    if should_distribute():
+        logging.debug("Using distributed PyTorch with {} backend".format(args.backend))
+        dist.init_process_group(backend=args.backend)
+
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+
+    # Define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.2860,), (0.3530,))])
+    # Download and load the training data
+    train_data = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', 
+                                     download=True, train=True, 
+                                     transform=transform)
+    # DistributedSampler partitions the training dataset among the worker processes
+    train_sampler = (
+        torch.utils.data.distributed.DistributedSampler(train_data)
+        if should_distribute()
+        else None)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, 
+                                              sampler=train_sampler, 
+                                              shuffle=True, **kwargs)
+
+    # Download and load the test data
+    testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, 
+                                    train=False, transform=transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=64, 
+                                             shuffle=True, **kwargs)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+    model = Net().to(device)
+
+    if is_distributed():
+        if use_cuda:
+            torch.cuda.set_device(torch.cuda.current_device())
+        model = nn.parallel.DistributedDataParallel(model)
+
+    # See: https://pytorch.org/docs/stable/optim.html#torch.optim.Adadelta
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.5)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_model.pt")
+
+
+if __name__ == "__main__":
+    main()
